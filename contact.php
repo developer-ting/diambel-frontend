b125172ae00e456978c83c24e608c688ce442ebf
pramodchauhanting <pramod.chouhan@ting.in>
2023-06-27 18:54:38 +0530

    image changes done

--- contact.php
+++ contact.php
@@ -16,108 +16,87 @@
 <body>
   <?php include("includes/browser_upgrade.html") ?>
   <?php include("includes/loader.html") ?>
-
+  <?php include("includes/header.html") ?>
   <?php include("includes/sidebar.html") ?>
 
   <!--  insert body content  -->
   <main id="contact_page" class="smooth_scroll_pages">
-    <p>Multiply every element in the array with 10:</p>
 
-    <p id="demo"></p>
   </main>
   <!--  end body content -->
 
 
   <?php include("includes/include_js.html") ?>
+  <?php include("includes/include_js.html") ?>
 
-  <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js">
-  </script> -->
-  <script>
-  //   const numbers = [34, 40, 56, 21];
-  //   const newArr = numbers.map(myNumber);
-
-  //   console.log(newArr);
-  //   document.getElementById("demo").innerHTML = newArr;
-
-  //   function myNumber(num) {
-  //     return num * 10;
-  //   }
-
-  const square = [4, 5, 6, 7];
-  const newSquare = square.map(mySquare);
-
-  console.log(newSquare);
-  document.getElementById("demo").innerHTML = newSquare;
-
-  function mySquare(squareNumber) {
-    return squareNumber * squareNumber;
-  }
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js">
   </script>
+
   <script>
-  // //for letters only
-  // $.validator.addMethod("lettersonly", function(value, element) {
-  //     return this.optional(element) || /^[a-zA-Z][a-zA-Z ]+$/i.test(value);
-  // });
+  //for letters only
+  $.validator.addMethod("lettersonly", function(value, element) {
+    return this.optional(element) || /^[a-zA-Z][a-zA-Z ]+$/i.test(value);
+  });
 
-  // //for email only
-  // $.validator.addMethod("emailtest", function(value, element) {
-  //     return this.optional(element) || /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/i.test(value);
-  // });
+  //for email only
+  $.validator.addMethod("emailtest", function(value, element) {
+    return this.optional(element) || /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/i.test(value);
+  });
 
 
-  // $("#enquiry-form").validate({
-  //     rules: {
-  //         fullname: {
-  //             required: true,
-  //             lettersonly: true,
-  //             minlength: 2
-  //         },
-  //         mobile: {
-  //             required: true,
-  //             minlength: 10,
-  //             maxlength: 10,
-  //             digits: true
-  //         },
-  //         email: {
-  //             emailtest: true,
-  //             required: true,
-  //             email: true
-  //         },
-  //         message: {
-  //             required: true
-  //         }
-  //     },
-  //     messages: {
-  //         fullname: {
-  //             required: "This field is required",
-  //             lettersonly: "Please enter a text only"
-  //         },
-  //         mobile: {
-  //             required: "This field is required",
-  //             minlength: "Please enter a valid mobile number",
-  //             maxlength: "Please enter a valid mobile number",
-  //             digits: "Please enter a digits only"
-  //         },
-  //         email: {
-  //             required: "This field is required",
-  //             emailtest: "Please enter a valid email address"
-  //         },
-  //         message: {
-  //             required: "This field is required"
-  //         }
-  //     }
-  // });
+  $("#enquiry-form").validate({
+    rules: {
+      fullname: {
+        required: true,
+        lettersonly: true,
+        minlength: 2
+      },
+      mobile: {
+        required: true,
+        minlength: 10,
+        maxlength: 10,
+        digits: true
+      },
+      email: {
+        emailtest: true,
+        required: true,
+        email: true
+      },
+      message: {
+        required: true
+      }
+    },
+    messages: {
+      fullname: {
+        required: "This field is required",
+        lettersonly: "Please enter a text only"
+      },
+      mobile: {
+        required: "This field is required",
+        minlength: "Please enter a valid mobile number",
+        maxlength: "Please enter a valid mobile number",
+        digits: "Please enter a digits only"
+      },
+      email: {
+        required: "This field is required",
+        emailtest: "Please enter a valid email address"
+      },
+      message: {
+        required: "This field is required"
+      }
+    }
+  });
 
-  // $("#enquiry-form").ajaxForm({
-  //     success: function($response) {
-  //         if ($response == 1) {
-  //             $('#thank-you-msg').show();
-  //             setTimeout(function() {
-  //                 $('#thank-you-msg').hide();
-  //             }, 5000);
-  //         }
-  //     }
-  // });
+  $("#enquiry-form").ajaxForm({
+    success: function($response) {
+      if ($response == 1) {
+        $('#thank-you-msg').show();
+        setTimeout(function() {
+          $('#thank-you-msg').hide();
+        }, 5000);
+      }
+    }
+  });
   </script>
 </body>
 
